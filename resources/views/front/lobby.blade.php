45d89b36d5083aff4831f94d513cb4865e611649
JulianBby <89559052+JulianBby@users.noreply.github.com>
2023-05-20 14:56:01 -0500

    creacion lobby

---
+++ resources/views/front/lobby.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Front</title>
+    <link rel="stylesheet" href="{{ url('libs/css/css/style-log.css') }}">
+    <link rel="stylesheet" href="{{ url('libs/libs/bootstrap-5.0.2-dist/css/bootstrap.css') }}">
+</head>
+<body>
+<header class="cabeza">
+    <nav class="navbar navbar-light bg-light" style="background-color:#f0f7da!important;">
+        <div class="container-fluid" >
+          <a class="navbar-brand" href="#">
+            <img src="{{url ('img/img/logo_golden.png') }}" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            Confecciones Golden
+          </a>
+          <ul class="nav justify-content-end">
+        </ul>
+        </div>
+      </nav>
+</header> <br><br>
+
+
+</body>
+</html>
